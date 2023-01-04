e92f576edb46001bf5eb4a07100641530be14fd0
Sinisa Stevanovic <sinisahamburg97@gmail.com>
2023-01-04 21:06:05 +0100

    Add basic viewport setup with a randomly generated image

--- Raytracer/Source/Raytracer.cpp
+++ Raytracer/Source/Raytracer.cpp
@@ -1,25 +1,78 @@
 #include "Haketon/Application.h"
 #include "Haketon/EntryPoint.h"
+#include "Haketon/Image.h"
+#include "Haketon/Random.h"
+#include "Haketon/Timer.h"
 
-class ExampleLayer : public Haketon::Layer
+using namespace Haketon;
+
+class ExampleLayer : public Layer
 {
 public:
     virtual void OnUIRender() override
     {
-        ImGui::Begin("Hello");
-        ImGui::Button("Button");
+        ImGui::Begin("Settings");
+        ImGui::Text("Last render: %.3fms", LastRenderTime_);
+        if (ImGui::Button("Render"))
+        {
+            Render();
+        }
         ImGui::End();
 
-        ImGui::ShowDemoWindow();
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+        ImGui::Begin("Viewport");
+
+        ViewportWidth_ = ImGui::GetContentRegionAvail().x;
+        ViewportHeight_ = ImGui::GetContentRegionAvail().y;
+
+        if(Image_)
+            ImGui::Image(Image_->GetDescriptorSet(), {(float)Image_->GetWidth(), (float)Image_->GetHeight()});
+
+        ImGui::End();
+        ImGui::PopStyleVar();
+
+        Render();
+
+        //ImGui::ShowDemoWindow();
     }
+
+    void Render()
+    {
+        Timer timer;
+        
+        if (!Image_ || ViewportWidth_ != Image_->GetWidth() || ViewportHeight_ != Image_->GetHeight())
+        {
+            Image_ = std::make_shared<Image>(ViewportWidth_, ViewportHeight_, ImageFormat::RGBA);
+            delete[] ImageData_;
+            ImageData_ = new uint32_t[ViewportWidth_ * ViewportHeight_];
+        }
+
+        for (uint32_t i = 0; i < ViewportWidth_ * ViewportHeight_; i++)
+        {
+            ImageData_[i] = Random::UInt();
+            ImageData_[i] |= 0xff000000;
+        }
+
+        Image_->SetData(ImageData_);
+
+        LastRenderTime_ = timer.ElapsedMs();
+    }
+
+private:
+    std::shared_ptr<Image> Image_;
+    uint32_t* ImageData_ = nullptr;
+    uint32_t ViewportWidth_ = 0;
+    uint32_t ViewportHeight_ = 0;
+
+    float LastRenderTime_ = 0.0f;
 };
 
-Haketon::Application* Haketon::CreateApplication(int argc, char** argv)
+Application* Haketon::CreateApplication(int argc, char** argv)
 {
-    Haketon::ApplicationSpecification spec;
-    spec.Name = "Haketon Project";
+    ApplicationSpecification spec;
+    spec.Name = "Raytracer";
 
-    Haketon::Application* app = new Haketon::Application(spec);
+    Application* app = new Application(spec);
     app->PushLayer<ExampleLayer>();
     app->SetMenubarCallback([app]()
     {
